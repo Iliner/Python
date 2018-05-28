35e1e57d65ff1eaf405923d3d08481517946b9ca
Ivan <mynamekasatkin@gmail.com>
2018-05-28 14:10:49 +0300

    last version

--- start.py
+++ start.py
@@ -5,19 +5,19 @@ import urllib.request
 import urllib3
 import re
 import csv
-
+import subprocess
 
 
 
 
 def csv_reader(file_obj):
        """
-       Создает словарь из CSV где ключ КОД а значения АРТИКУЛ
+       Создает словарь из CSV где ключ КОД(будет именем фото) а значения АРТИКУЛ(будет указателем для место выгрузки фото)
        """
        reader = csv.reader(file_obj)
        #dict_code_articul = dict()
        for row in reader:
-              if(row[2] == 'Yato'):
+              if(row[2] == 'Yato'): # Отдельный блок потому что мы ято ищем на другом сайте 
                      dict_code_articul = dict()
                      dict_code_articul.update({row[1] : row[3]})
                      producer = row[2]
@@ -31,6 +31,9 @@ def csv_reader(file_obj):
        #yato(dict_code_articul)
        #for key, value in dict_code_articul.items():
        #main(dict_code_articul)
+       crop_min()
+       crop_min_micro()
+       print('start.py ЗАКОНЧИЛ СВОЮ РАБОТУ')
 
 
 def many_photo(dict, producer):
@@ -56,10 +59,10 @@ def many_photo(dict, producer):
                                    try:
                                           div = soup.find('div', {'class': 'image'}).next
                                           href_photo = div.get('href')
-                                          print('many_photo ' + str(div))
+                                          #print('many_photo ' + str(div))
                                           dowanload(href_photo, code, producer)
                                    except AttributeError as err:
-                                          print('нет такого изображение пропускаем  ' + key + value)
+                                          #print('нет такого изображение пропускаем  ' + key + value)
                                           continue
 def yato(dict, producer):
        """
@@ -77,6 +80,9 @@ def yato(dict, producer):
 
 
 def dowanload(data, key, producer):
+       """
+       Функция непосредственно скачивает фото
+       """
        url = data
        get_photo = requests.get(url)
        #format_photo = url.rsplit('.', maxsplit=1)
@@ -85,15 +91,68 @@ def dowanload(data, key, producer):
        format_photo = url.rsplit('.', maxsplit=1)[-1]
        full_name = key + "." + format_photo
        producer_1 = producer.capitalize()
-       producer_2 = producer.lower()
-       print(producer_2 + '_bot' '/' + full_name)
-       file = open('/var/www/opt-online/photos/' + producer_2 + '_bot' '/' + full_name, 'wb')
-       #file = open('./test/' + full_name, 'wb')
-       #file = open('./Yato_img/' + full_name, 'wb')
-       file.write(get_photo.content)
-       #print(data, key,)
-       file.close()
+       """
+       Меняем имена так как они указаны с учетом из название в репозитории 
+       """
+       if producer == 'D&D' or producer == 'd&d':
+              producer = 'dandd'
+       elif producer == 'Юпитер':
+              producer = 'jupiter'
+       elif producer == 'Белстаб':
+              producer = 'belstub'
+       elif producer == 'KingTul kraft':
+              producer = 'kingtulkraft'
+       elif producer == 'big red':
+            producer = 'bigred'
+       elif producer == 'KINGTUL profi':
+              producer = 'kingtulprofi'
+       elif producer == 'ПрофКлей':
+              producer = 'profcley'
+       elif producer == 'HÖGERT technik':
+              producer = 'hogert'
+       elif producer == 'ПрофКлей':
+              producer = 'profcley' 
 
+
+       producer_2 = producer.lower()
+       producer_2 = producer_2.replace(' ', '')
+       print(producer_2 + '_bot' '/' + full_name)
+       try:
+              file = open('/var/www/opt-online/photos/bot_producer/' + producer_2 + '_bot' '/' + full_name, 'wb')
+              #file = open('./test/' + full_name, 'wb')
+              #file = open('./Yato_img/' + full_name, 'wb')
+              file.write(get_photo.content)
+              #print(data, key,)
+              file.close()
+              
+       except FileNotFoundError as err:
+              print(err)
+              
+
+def crop_min():
+       print('crop_min я начал работу')
+       list_photo = ['baumauto', 'belstub', 'bigred', 'braumauto', 
+                     'dandd', 'forcekraft', 'forsage', 'forsageelectro', 
+                     'geko', 'groz', 'gtv', 'h-d', 'hammer', 'hcb', 'hogert', 
+                     'jupiter', 'kingtul', 'kingtulkraft', 'kingtulprofi', 'krino', 
+                     'luxi', 'm7', 'marshal', 'partner', 'profcley', 'prowin', 'rotake', 
+                     'selta', 'telwin', 'wortex', 'yato']
+
+       for producer in list_photo:
+              subprocess.call(["bash", "/var/www/opt-online/sh/crop_for_min.sh", producer])
+
+
+def crop_min_micro():
+       print('crop_min_micro я начал работу')
+       list_photo = ['baumauto', 'belstub', 'bigred', 'braumauto', 
+                     'dandd', 'forcekraft', 'forsage', 'forsageelectro', 
+                     'geko', 'groz', 'gtv', 'h-d', 'hammer', 'hcb', 'hogert', 
+                     'jupiter', 'kingtul', 'kingtulkraft', 'kingtulprofi', 'krino', 
+                     'luxi', 'm7', 'marshal', 'partner', 'profcley', 'prowin', 'rotake', 
+                     'selta', 'telwin', 'wortex', 'yato']
+
+       for producer in list_photo:
+              subprocess.call(["bash", "/var/www/opt-online/sh/crop_for_min_micro.sh", producer])
 
 """
 def main():
@@ -109,6 +168,6 @@ def main():
 """
 
 if __name__ == '__main__':
-       csv_path = "OptOnline.csv"
+       csv_path = "opt_clear.csv"
        with open(csv_path, "r") as f_obj:
               csv_reader(f_obj)
